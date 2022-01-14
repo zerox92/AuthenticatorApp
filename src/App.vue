e139c6eafdb1a5fe96f71c81a7f360ab16a1ede9
System Administrator <root@88665a0cd311.ant.amazon.com>
2022-01-14 16:10:04 +0000

    App JS

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Amplify from 'aws-amplify';
+  import awsconfig from './aws-exports';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  Amplify.configure(awsconfig);
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+</template>
